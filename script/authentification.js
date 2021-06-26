c7431079af3d2ec2c4985b69a57f984fbeb941ec
Elmabi <home@home.com>
2021-06-26 14:13:14 -0400

    Change files path

--- script/authentification.js
+++ script/authentification.js
@@ -1,5 +1,5 @@
 var usernamePassword = [];
-fetch("/MyPortfolio/authentification/authentification.txt")
+fetch("../authentification/authentification.txt")
   .then((response) => response.text())
   .then((data) => {
     usernamePassword = data.split(",");
@@ -13,41 +13,41 @@ document.querySelector("button").onclick = () => {
   }
 
   let lien = document.createElement("a");
-  lien.href = "/MyPortfolio/main.html";
+  lien.href = "../main.html";
   document.body.appendChild(lien);
   lien.click();
   lien.remove();
 };
 
-document.addEventListener(
-  "keydown",
-  function (event) {
-    if (event.keyCode == 123) {
-      alert("Cette fonction a été désactivée !");
-      return false;
-    } else if (event.ctrlKey && event.shiftKey && event.keyCode == 73) {
-      alert("Cette fonction a été désactivée !");
-      return false;
-    } else if (event.ctrlKey && event.keyCode == 85) {
-      alert("Cette fonction a été désactivée !");
-      return false;
-    }
-  },
-  false
-);
+// document.addEventListener(
+//   "keydown",
+//   function (event) {
+//     if (event.keyCode == 123) {
+//       alert("Cette fonction a été désactivée !");
+//       return false;
+//     } else if (event.ctrlKey && event.shiftKey && event.keyCode == 73) {
+//       alert("Cette fonction a été désactivée !");
+//       return false;
+//     } else if (event.ctrlKey && event.keyCode == 85) {
+//       alert("Cette fonction a été désactivée !");
+//       return false;
+//     }
+//   },
+//   false
+// );
 
-if (document.addEventListener) {
-  document.addEventListener(
-    "contextmenu",
-    function (e) {
-      alert("Cette fonction a été désactivée !");
-      e.preventDefault();
-    },
-    false
-  );
-} else {
-  document.attachEvent("oncontextmenu", function () {
-    alert("Cette fonction a été désactivée !");
-    window.event.returnValue = false;
-  });
-}
+// if (document.addEventListener) {
+//   document.addEventListener(
+//     "contextmenu",
+//     function (e) {
+//       alert("Cette fonction a été désactivée !");
+//       e.preventDefault();
+//     },
+//     false
+//   );
+// } else {
+//   document.attachEvent("oncontextmenu", function () {
+//     alert("Cette fonction a été désactivée !");
+//     window.event.returnValue = false;
+//   });
+// }
